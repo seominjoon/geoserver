1e79637f5cdbba09db15e34b0397a53f0ec5622d
Minjoon Seo <seominjoon@gmail.com>
2015-04-16 00:03:05 -0700

    updated run.sh

--- geoserver/run.sh
+++ geoserver/run.sh
@@ -1,8 +1,8 @@
-export PYTHONPATH=$PYTHONPATH:/Library/Python/2.7/site-packages
-export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python2.7/site-packages
-export PYTHONPATH=$PYTHONPATH:/usr/local/lib/wxPython-3.0.0.0/lib/python2.7/site-packages
-export PYTHONPATH=$PYTHONPATH:/usr/local/lib/wxPython-3.0.0.0/lib/python2.7/site-packages/wx-3.0-osx_cocoa
+# export PYTHONPATH=$PYTHONPATH:/Library/Python/2.7/site-packages
+# export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python2.7/site-packages
+# export PYTHONPATH=$PYTHONPATH:/usr/local/lib/wxPython-3.0.0.0/lib/python2.7/site-packages
+# export PYTHONPATH=$PYTHONPATH:/usr/local/lib/wxPython-3.0.0.0/lib/python2.7/site-packages/wx-3.0-osx_cocoa
 #export PYTHONPATH=$PYTHONPATH:/Users/minjoon/Documents/workspace/GeoSolver
-export PYTHONPATH=$PYTHONPATH:/Users/minjoon/Documents/workspace/TinyOCR
-export PYTHONPATH=$PYTHONPATH:/Users/minjoon/PycharmProjects/geosolver
-django-admin.py $1 $2 --settings=geoserver.settings.local
+# export PYTHONPATH=$PYTHONPATH:/Users/minjoon/Documents/workspace/TinyOCR
+# export PYTHONPATH=$PYTHONPATH:/Users/minjoon/PycharmProjects/geosolver
+python manage.py $1 $2 --settings=geoserver.settings.local

ede4ec3fcdf82a2babd3895f1b79909c51b6a039
Minjoon Seo <seominjoon@gmail.com>
2015-05-06 17:44:21 -0700

    checking validity of input parse now

--- geoserver/semantics/views.py
+++ geoserver/semantics/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import View, ListView
 from geoserver.utils import get_next_item
+from geosolver.text.annotation_to_node import is_valid_annotation
 from questions.models import Question, Sentence, QuestionTag
 from questions.views import _get_queries, _filter_questions
 from semantics.forms import SentenceParseForm
@@ -29,36 +30,45 @@ class SentenceParseAnnotateView(View):
         form = SentenceParseForm(request.POST)
         if form.is_valid():
             lines = form.cleaned_data['parses'].split('\r\n')
+            valid = True
+            invalid_lines = []
             for index, line in enumerate(lines):
                 line = line.rstrip().lstrip()
-                semantic_parse = SemanticParse(sentence=sentence, number=index, parse=line)
-                semantic_parse.save()
+                if not is_valid_annotation(line):
+                    valid = False
+                    invalid_lines.append(line)
 
-            if len(question.sentences.all()) - 1 == sentence.index:
-                pk_list = [q.pk for q in Question.objects.filter(tags__word="unannotated")]
-                if QuestionTag.objects.filter(word="unannotated").exists() and QuestionTag.objects.filter(word="annotated").exists():
-                    unannotated = QuestionTag.objects.get(word="unannotated")
-                    annotated = QuestionTag.objects.get(word="annotated")
-                    question.tags.remove(unannotated)
-                    question.tags.add(annotated)
-                    question.save()
-                next_question_pk = pk_list[pk_list.index(int(question_pk)) + 1]
-                next_sentence_index = 0
-            else:
-                next_question_pk = question_pk
-                next_sentence_index = "%d" % (int(sentence_index) + 1)
+            if valid:
+                for index, line in enumerate(lines):
+                    line = line.rstrip().lstrip()
+                    semantic_parse = SemanticParse(sentence=sentence, number=index, parse=line)
+                    semantic_parse.save()
 
-            kwargs = {'question_pk': next_question_pk, 'sentence_index': next_sentence_index}
-            data = {'title': 'Success',
-                    'message': 'Semantic parses annotated successfully.',
-                    'link': reverse('semantics-annotate', kwargs=kwargs),
-                    'linkdes': 'Annotate the next sentence.'}
-            return render(request, 'result.html', data)
-        else:
-            data = {'title': 'Failed',
-                    'message': form.errors(),
-                    'linkdes': 'Go back and upload the tree again.'}
-            return render(request, 'result.html', data)
+                if len(question.sentences.all()) - 1 == sentence.index:
+                    pk_list = [q.pk for q in Question.objects.filter(tags__word="unannotated")]
+                    if QuestionTag.objects.filter(word="unannotated").exists() and QuestionTag.objects.filter(word="annotated").exists():
+                        unannotated = QuestionTag.objects.get(word="unannotated")
+                        annotated = QuestionTag.objects.get(word="annotated")
+                        question.tags.remove(unannotated)
+                        question.tags.add(annotated)
+                        question.save()
+                    next_question_pk = pk_list[pk_list.index(int(question_pk)) + 1]
+                    next_sentence_index = 0
+                else:
+                    next_question_pk = question_pk
+                    next_sentence_index = "%d" % (int(sentence_index) + 1)
+
+                kwargs = {'question_pk': next_question_pk, 'sentence_index': next_sentence_index}
+                data = {'title': 'Success',
+                        'message': 'Semantic parses annotated successfully.',
+                        'link': reverse('semantics-annotate', kwargs=kwargs),
+                        'linkdes': 'Annotate the next sentence.'}
+                return render(request, 'result.html', data)
+
+        data = {'title': 'Failed',
+                'message': "%r" % (invalid_lines),
+                'linkdes': 'Go back and upload the tree again.'}
+        return render(request, 'result.html', data)
 
 
 

e523e2cdc30987a238f6ad400c6beb706a182a7a
Minjoon Seo <seominjoon@gmail.com>
2015-08-27 18:12:37 -0700

    added semantics dump

--- geoserver/dump.sh
+++ geoserver/dump.sh
@@ -1,5 +1,6 @@
 mkdir $1/$2
 ./run.sh dumpdata questions > $1/$2/questions.json
 ./run.sh dumpdata labels > $1/$2/labels.json
+./run.sh dumpdata semantics > $1/$2/semantics.json
 tar -zcf $1/$2/media.tar.gz media/
 
